9f889fc924eab5f702ede8340dfb020ab68bdaae
Charles King <bringking@gmail.com>
2018-05-24 15:45:43 -0700

    Add random

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const router = new Router();
 // Static files
 app.use(require("koa-static")(__dirname + "/static"));
 // Api
-router.post("/api", koaBody(), async ({ request, response }, next) => {
+router.post("/api/sha", koaBody(), async ({ request, response }, next) => {
   const sha = request.body.sha;
   const name = await fetch(
     `http://release-name:6767/api/release-name/${sha}`
@@ -16,6 +16,14 @@ router.post("/api", koaBody(), async ({ request, response }, next) => {
   response.body = { name };
   response.status = 201;
 });
+
+router.get("/api/random", async ({ request, response }, next) => {
+  const { data } = await fetch(
+    `http://release-name:6767/api/release-name/random`
+  ).then(res => res.json());
+  response.body = { name: data.name };
+});
+
 // Setup Routes
 app.use(router.routes()).use(router.allowedMethods());
 // Start the server
